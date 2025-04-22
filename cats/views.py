c188e023a1b592f1691f60d8e58ed54ef2edb83f
Ndk985 <118891786+Ndk985@users.noreply.github.com>
2025-04-22 15:42:28 +0300

    kittygram2plus_v.3

--- cats/views.py
+++ cats/views.py
@@ -1,20 +1,35 @@
 from rest_framework import viewsets
-
 from .models import Achievement, Cat, User
-
 from .serializers import AchievementSerializer, CatSerializer, UserSerializer
 from .permissions import OwnerOrReadOnly, ReadOnly
 from rest_framework.throttling import AnonRateThrottle
 from .throttling import WorkingHoursRateThrottle
 from rest_framework.throttling import ScopedRateThrottle
+from rest_framework.pagination import PageNumberPagination
+from rest_framework.pagination import LimitOffsetPagination
+from .pagination import CatsPagination
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import filters
 
 
 class CatViewSet(viewsets.ModelViewSet):
     queryset = Cat.objects.all()
     serializer_class = CatSerializer
     permission_classes = (OwnerOrReadOnly,)
-    throttle_classes = (WorkingHoursRateThrottle, ScopedRateThrottle)
-    throttle_scope = 'low_request' 
+    # throttle_classes = (WorkingHoursRateThrottle, ScopedRateThrottle)
+    # throttle_scope = 'low_request'
+    # pagination_class = CatsPagination
+    pagination_class = None
+    filter_backends = (
+        DjangoFilterBackend,
+        filters.SearchFilter,
+        filters.OrderingFilter
+    )
+    filterset_fields = ('color', 'birth_year')
+    search_fields = ('name',)
+    ordering_fields = ('name', 'birth_year')
+    ordering = ('birth_year',)
+
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
